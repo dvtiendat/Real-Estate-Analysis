a849f84d6281039185767cac848f081325b45da6
dvtiendat <111187020+dvtiendat@users.noreply.github.com>
2024-10-22 14:35:19 +0700

    Fixed logger in runner.py

--- WebCrawler/runner.py
+++ WebCrawler/runner.py
@@ -5,16 +5,17 @@ import logging
 import pandas as pd
 import yaml
 import os
+
+# setup logger
 os.makedirs('logs', exist_ok=True)
-
-logging.basicConfig(
-    filename='logs/bds_com_logs',
-    encoding='utf-8',
-    filemode='a',
-    level=logging.INFO, 
-    format='%(asctime)s - %(levelname)s - %(message)s')
-
-logger = logging.getLogger(__name__)
+log_filename = 'logs/bds_com.log'
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler(filename=log_filename, encoding='utf-8')
+file_handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 def get_config(path):
     '''
