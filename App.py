289662fd126ac2ba9281cf7ea9bc2f17f9386b7d
DucNgoQuang <ducngoquang275@gmail.com>
2024-12-08 10:19:40 +0700

    Update App.py

--- App.py
+++ App.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd 
 import matplotlib.pyplot as plt
 import seaborn as sns
-import plotly
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import sys
